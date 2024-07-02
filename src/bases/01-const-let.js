afb034e63bf8d787af107f63c6829ca3cc990a8b
Florencia Fernandez <96849628+FlorenciaF1@users.noreply.github.com>
2024-07-02 18:21:02 -0300

    React

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,21 @@
+
+// Variables y Constantes
+
+const nombre = 'Florencia`';
+const apellido = 'Fernandez';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado )
+
+// var NO ya no se utiliza actualmente
+if (true) {
+    const nombre = 'Peter';
+    console.log(nombre)
+
+    let valorDado = 6;
+    console.log(valorDado)
+}
+
+console.log (valorDado, nombre)
