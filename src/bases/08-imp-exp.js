b7b5f7ae05c2228c45af6766d3ed9f693caba10d
Florencia Fernandez <96849628+FlorenciaF1@users.noreply.github.com>
2024-07-11 19:00:14 -0300

    08-imp-exp

---
+++ src/bases/08-imp-exp.js
@@ -0,0 +1,37 @@
+
+import heroes, {owners} from '../data/heroes.js';
+
+console.log( heroes );
+console.log( owners );
+
+// el .find() recibe una funcion como argumento llamada callback
+
+// Tarea
+// Devolver al heroe que querramos segun su id
+
+// Mejor solucion, dejo dos alternativas abajo de todo:
+const getHeroById = (id) => {
+    return heroes.find( ( heroe ) => heroe.id === id);
+}
+
+console.log ( getHeroById(3) );
+
+// Tarea
+// Devolver todos los heroes que tengan DC de owner
+const getHeroesByOwner = ( owner ) => heroes.filter( ( heroe ) => heroe.owner === owner);
+
+console.log ( getHeroesByOwner('DC') );
+
+// Solucion mas larga:
+// const getHeroById = (id) => {
+//     return heroes.find( (heroe) => {
+//         if (heroe.id === id) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     });
+// }
+
+// Solucion mas corta:
+// const getHeroById = (id) => heroes.find( ( heroe ) => heroe.id === id);
